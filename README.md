4c2ec38a746657d9c19af1bfa139aa401b900de8
Parker Conrad <parkerconrad814@yahoo.com>
2020-11-10 15:36:37 -0800

    added choose color feature

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Etch a Sketch
-
+My version of the childhood classic, etch-a-sketch, for The Odin Project
 ## Live Preview
 https://parkercon.github.io/etch-a-sketch/
+## Clone this repository
+'git clone https://github.com/parkercon/rock-paper-scissors.git'
+## Note
+Unfortunately, as of this time, there is no mobile support. Hopefully soon!
+# Enjoy! 
--- script.js
+++ script.js
@@ -74,6 +74,17 @@ black.addEventListener('click', function(){
     }
 });
 
+const chooseColor = document.querySelector('#color');
+chooseColor.addEventListener('input', function(){
+    let val = document.getElementById('slider').value;
+    let newColor = document.getElementById('color').value;
+    let cell = grid.children;
+    for (let i = 0; i < val*val; i++) {
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = newColor;
+        })
+    }
+});
 
 
 
